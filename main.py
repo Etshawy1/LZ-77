f712d47ae4879839c640b3af5735d4f5329e274d
Etshawy1 <muhammad.mahmoud99@eng-st.cu.edu.eg>
2020-04-30 17:23:39 +0200

    improving matching sequence performance

--- main.py
+++ main.py
@@ -2,20 +2,20 @@
 Created on Sat Apr 25 22:08:19 2020
 @author: etshawy
 """
+
+import os
 import math
 import numpy as np
 import cv2
-import more_itertools
 import user_input
 
 min_acceptable_sequence_length = 2
 data_type = 'B'
-img = user_input.read_gray_img()
+img, img_path = user_input.read_gray_img()
 height, width = img.shape
 window_size = user_input.input_int('please enter the window size: ')
-if window_size > 255:
-    data_type = 'uint16' # data type of the prefixes of the tags (offset, length)
-lookahead_buffer_size = user_input.input_int('please enter the look ahead buffer size: ')
+lookahead_buffer_size = user_input.input_int(
+    'please enter the look ahead buffer size: ')
 if lookahead_buffer_size > window_size:
     print("buffer can't be greater than the whole window, terminating program...")
     exit(-1)
@@ -23,11 +23,12 @@ if lookahead_buffer_size > window_size:
 flattened = img.flatten().tolist()
 search_buffer_size = window_size - lookahead_buffer_size
 
+
 def get_longest_match(data, current_position):
     """Summary or Description of the Function
 
     Parameters:
-    data (array of int): the flattened image pixel values
+    data (array of uint8): the flattened image pixel values
     current_position (int): the index of the start of the lookahead buffer
 
     Returns:
@@ -36,7 +37,8 @@ def get_longest_match(data, current_position):
    """
 
     minimum_search_index = max(0, current_position - search_buffer_size - 1)
-    maximum_search_index = min(len(flattened) - 1, current_position + lookahead_buffer_size - 1)
+    maximum_search_index = min(
+        len(flattened) - 1, current_position + lookahead_buffer_size - 1)
     search_buffer = data[minimum_search_index: maximum_search_index + 1]
     lookahead_buffer = data[current_position: maximum_search_index + 1]
     l = min_acceptable_sequence_length
@@ -46,37 +48,44 @@ def get_longest_match(data, current_position):
     while l <= r:
         mid = l + (r - l)//2
         # get match index of legth = mid
-        test_match_index = match_sequence(search_buffer, lookahead_buffer[: mid], minimum_search_index, current_position)
+        test_match_index = match_sequence(
+            search_buffer, lookahead_buffer[: mid], minimum_search_index, current_position)
         # there is a match with length = mid so test if there is higher length
-        if test_match_index != -1:  
+        if test_match_index != -1:
             l = mid + 1
             match_index = test_match_index
             match_length = mid
         # check for lower match lengths
-        else: 
+        else:
             r = mid - 1
 
     if match_index != -1:
         offset = current_position - match_index
+        if offset > 255 or match_length > 255:
+            data_type = 'uint16'
         return (offset, match_length)
     return (0, 0)
 
 
 def match_sequence(arr, sequence, min_indx, current_pos):
-    possible_matches = list(more_itertools.windowed(arr, len(sequence)))
-    for sequence_start_index in range(current_pos - min_indx + 1):
-        if sequence_start_index + min_indx >= current_pos:
-            return -1
-        if list(possible_matches[sequence_start_index]) == sequence:
-            return (sequence_start_index + min_indx)
-    return (-1)
-
+    sequence_index = 0
+    search_buffer_current_size = current_pos - min_indx
+    for i in range(search_buffer_current_size):
+        for j in range(len(sequence)):
+            if arr[i+j] == sequence[sequence_index]:
+                sequence_index += 1
+                if sequence_index == len(sequence):
+                    return (i + min_indx)
+            else:
+                sequence_index = 0
+                break
+    return -1
 
 
 current_position = 0
 codes = []  # store the offset and length of each tag
 flags = []  # flag to indicate if next is a tag or just a charachter code
-pixels = [] # pixel values in tags or without tags depends on the corresponding flag
+pixels = []  # pixel values in tags or without tags depends on the corresponding flag
 print('encoding...')
 while current_position < len(flattened):
     offset, length = get_longest_match(flattened, current_position)
@@ -105,10 +114,6 @@ flags = np.packbits(flags_before, axis=None)
 np.save('image.npy', pixels)
 np.save('prefixes.npy', codes)
 np.save('flags.npy', flags)
-
-# printed to check the performace of the compression
-#print(len(flattened))
-#print(len(flags_before))
 
 
 print('decoding...')
@@ -152,5 +157,11 @@ decoded_img = decoded_img.astype(np.uint8)
 # show the decoded image and store it as output.png
 cv2.imshow('Gray image', decoded_img)
 cv2.imwrite('output.png', decoded_img)
+compressed_size = 0
+compressed_size += os.path.getsize(os.getcwd() + '\\flags.npy')
+compressed_size += os.path.getsize(os.getcwd() + '\\image.npy')
+compressed_size += os.path.getsize(os.getcwd() + '\\prefixes.npy')
+print('compressed file size = ', compressed_size)
+print('original image size: ', os.path.getsize(img_path))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- user_input.py
+++ user_input.py
@@ -5,18 +5,20 @@ Created on Mon Apr 13 22:50:56 2020
 
 @author: etshawy
 """
-import  cv2
+import cv2
+
 
 def input_int(message):
-  while True:
-    try:
-       userInput = int(input(message))       
-    except ValueError:
-       print("Not an integer! Try again.")
-       continue
-    else:
-       return userInput 
-       break 
+    while True:
+        try:
+            userInput = int(input(message))
+        except ValueError:
+            print("Not an integer! Try again.")
+            continue
+        else:
+            return userInput
+            break
+
 
 def read_gray_img():
     while True:
@@ -25,4 +27,4 @@ def read_gray_img():
         if not img.any():
             print("please provide a right path to image, terminating...")
         else:
-            return img
+            return (img, path)
